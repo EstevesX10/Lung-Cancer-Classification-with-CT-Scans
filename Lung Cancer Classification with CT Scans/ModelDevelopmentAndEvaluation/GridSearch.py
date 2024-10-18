81402cc284e3501879f54b73dfe12fdb5caf2cfd
Gongas <goncalo.mesteves03@gmail.com>
2024-10-18 20:58:49 +0100

    Updated the Grid Search to use a scoring method and also to save the best estimator

--- Lung Cancer Classification with CT Scans/ModelDevelopmentAndEvaluation/GridSearch.py
+++ Lung Cancer Classification with CT Scans/ModelDevelopmentAndEvaluation/GridSearch.py
@@ -1,10 +1,12 @@
 import pandas as pd
-from sklearn.model_selection import (GridSearchCV)
 from sklearn.base import (BaseEstimator)
+from sklearn.model_selection import (GridSearchCV)
+from sklearn.metrics import (SCORERS)
 import xgboost as xgb
 from .jsonFileManipulation import (dictToJsonFile, jsonFileToDict)
+from .pickleBestEstimatorsManagement import (saveBestEstimator)
 
-def isMachineLearningModel(model:any) -> bool:
+def isMachineLearningModel(model:object) -> bool:
     """
     # Description
         -> This function checks if a given instance
@@ -14,7 +16,7 @@ def isMachineLearningModel(model:any) -> bool:
     """
     return isinstance(model, (BaseEstimator, xgb.XGBModel))
 
-def computeModelBestParameters(model:any, parameterGrid:dict, X:pd.DataFrame, y:pd.DataFrame, modelPathsConfig:dict, saveBestParams:bool) -> dict:
+def computeModelBestParameters(model:object, parameterGrid:dict, X:pd.DataFrame, y:pd.DataFrame, scoring:str, modelPathsConfig:dict, saveBestModel:bool) -> dict:
     """
     # Description
         -> This funtion aims to calculate the best parameters of a given model using a Grid Search approach.
@@ -23,9 +25,14 @@ def computeModelBestParameters(model:any, parameterGrid:dict, X:pd.DataFrame, y:
     := param: parameterGrid - Hyperparameter values to consider when performing Grid Search.
     := param: X - Features from the dataset.
     := param: y - Target label from the dataset.
+    := param: scoring - Evaluation metric to take into consideration when performing Grid Search.
     := param: modelPathsConfig - Dictionary with the file paths to save the model best parameters.
-    := param: saveBestParams - Boolean value that determined whether or not to save the best parameters of the model.
+    := param: saveBestModel - Boolean value that determined whether or not to save the best model and its parameters.
     """
+
+    # Check if the given scoring method is valid
+    if scoring not in SCORERS:
+        raise ValueError("Got Invalid Scoring!")
 
     # Instanciate the classifier
     classifier = model(random_state=42)
@@ -35,7 +42,7 @@ def computeModelBestParameters(model:any, parameterGrid:dict, X:pd.DataFrame, y:
         raise ValueError("The given model does not match a Machine Learning Model!")
 
     # Initialize a instance of the grid search
-    gridSearch = GridSearchCV(estimator=classifier, param_grid=parameterGrid[type(classifier).__name__], refit=True, cv=3, n_jobs=-1, verbose=3)
+    gridSearch = GridSearchCV(estimator=classifier, param_grid=parameterGrid[type(classifier).__name__], scoring=scoring, refit=True, cv=3, n_jobs=-1, verbose=3)
 
     # Perform grid search
     gridSearch.fit(X, y)
@@ -43,9 +50,13 @@ def computeModelBestParameters(model:any, parameterGrid:dict, X:pd.DataFrame, y:
     # Retrive the best parameters
     bestParameters = gridSearch.best_params_
 
-    # Save the best parameters
-    if saveBestParams:
-        dictToJsonFile(bestParameters, filePath=modelPathsConfig[type(classifier).__name__]['bestParamsPath'])
+    # Save if the model is to be saved
+    if saveBestModel:
+        # Save the best parameters
+        dictToJsonFile(bestParameters, filePath=modelPathsConfig[type(classifier).__name__][scoring]['bestParamsPath'])
+
+        # Save the best estimator
+        saveBestEstimator(bestEstimator=gridSearch.best_estimator_, filePath=modelPathsConfig[type(classifier).__name__][scoring]['bestEstimatorPath'])
 
     # Return the dictionary with the best parameters
     return bestParameters
